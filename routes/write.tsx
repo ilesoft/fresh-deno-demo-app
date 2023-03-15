17f9262a83e0c831231a68e6f7e3537ae7bf8017
Ile <ilmari.marttila@gmail.com>
2023-03-15 09:37:12 +0200

    Better Write component

--- routes/write.tsx
+++ routes/write.tsx
@@ -2,17 +2,18 @@ import { Handlers } from "$fresh/server.ts";
 
 export const handler: Handlers<void> = {
   GET(req: Request, ctx) {
-    console.log(req)
+    console.log(req.url)
     return ctx.render();
   },
 };
 
-export default function Page() {
+export default function Write() {
   return (
       <div>
         <p><a href={"/"}>takaasin kotiin</a></p>
         <form method="GET">
           <label for="desc">Kuvaus tehdystä työstä</label>
+          <br/>
           <input type="text" name="desc" />
           <br/>
           <label for="amount">Paljos teit töitä</label>
